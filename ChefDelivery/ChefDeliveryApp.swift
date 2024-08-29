05bf77c0f4e6bc272d7d50658b9ff821cbff27af
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2024-08-29 16:43:00 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Gabriel Hora on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by Gabriel Hora on 29/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
